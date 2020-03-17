475f2f0c461944bf7a327107d64e75356fd19538
autumn-ragland <raglanda998@gmail.com>
2020-03-17 10:31:46 -0500

    keep up with form submissions

--- form-lecture-app/src/components/StudentForm.jsx
+++ form-lecture-app/src/components/StudentForm.jsx
@@ -4,12 +4,28 @@ class StudentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            studentName: " ",
+            studentGrade: 0,
+            studentArray: [],
         }
     }
 
     handleSubmission = (event) => {
         event.preventDefault();
+        this.state.studentArray.push(
+            {
+                name: this.state.studentName,
+                grade: this.state.studentGrade
+            });
+        console.log(this.state.studentArray);
+    }
+
+    handleNameChange = (event) => {
+        this.setState({ studentName: event.target.value });
+    }
+
+    handleGradeChange = (event) => {
+        this.setState({ studentGrade: event.target.value });
     }
 
     render() {
@@ -19,12 +35,12 @@ class StudentForm extends Component {
                 <form action="">
                     <fieldset>
                         <label htmlFor="studentName">Student Name : </label>
-                        <input type="text" name="studentName" id="studentName" />
+                        <input type="text" name="studentName" id="studentName" value={this.state.studentName} onChange={this.handleNameChange} />
 
                         <label htmlFor="studentGrade">Student Grade : </label>
-                        <input type="number" name="studentGrade" id="studentGrade" />
+                        <input type="number" name="studentGrade" id="studentGrade" value={this.state.studentGrade} onChange={this.handleGradeChange} />
 
-                        <button onClick = {this.handleSubmission}>Submit</button>
+                        <button onClick={this.handleSubmission}>Submit</button>
                     </fieldset>
                 </form>
             </div>
